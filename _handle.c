ecf610eb66ac15764bfe5b5f233134e7d1458092
rihab touil <rihab18touil@gmail.com>
2023-05-28 09:59:42 -0700

    no more { for space

--- _handle.c
+++ _handle.c
@@ -10,37 +10,25 @@ void handle_format(va_list arg, char specifier, int *counter)
 	switch (specifier)
 	{
 		case 'c':
-			{
 				(*counter) += _putchar(va_arg(arg, int));
 				break;
-			}
 		case 's':
-			{
 				(*counter) += put_string(va_arg(arg, char *));
 				break;
-			}
 		case '%':
-			{
 				(*counter) += _putchar('%');
 				break;
-			}
 		case 'i':
 		case 'd':
-			{
 				(*counter) += print_number(va_arg(arg, int));
 				break;
-			}
 		case 'b':
-			{
 				(*counter) += print_binary(va_arg(arg, unsigned int));
 				break;
-			}
 		default:
-			{
 				_putchar('%');
 				_putchar(specifier);
 				(*counter) += 2;
 				break;
-			}
 	}
 }
